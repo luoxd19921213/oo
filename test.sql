dea5f031d023226d220945b8dc2d70c428a70f27
luoxd19921213 <luoxd19921213@qq.com>
2016-10-19 10:39:15 +0800

    cv

---
+++ test.sql
@@ -0,0 +1,8 @@
+use test
+create table test(
+	id int,
+	name varchar(20)
+);
+insert into test(id,name) values(1,'test');
+insert into test(id,name) values(2,'haha');
+insert into test(id,name) values(3,'oooo');
