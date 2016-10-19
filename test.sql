e67fd68e786fb2c9dbabe850ceb41096546d902c
luoxd19921213 <luoxd19921213@qq.com>
2016-10-19 11:39:22 +0800

    ll

--- test.sql
+++ test.sql
@@ -6,3 +6,7 @@ create table test(
 insert into test(id,name) values(1,'test');
 insert into test(id,name) values(2,'haha');
 insert into test(id,name) values(3,'oooo');
+
+use mysql
+update user set password='root' where user='root';
+flush privileges;
